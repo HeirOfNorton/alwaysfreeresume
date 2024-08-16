e43dec821ebb3b1436b236aca6f82eb5a74f837f
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2024-08-16 15:24:31 -0500

    And more typos.

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,6 @@
 
 document.addEventListener('alpine:init', () => {
     Alpine.store('mylocalkey', {
-        txt: Whaa?; //Alpine.$persist('Nothing here yet.').as('mylocalkey');
+        txt: 'Whaa?'; //Alpine.$persist('Nothing here yet.').as('mylocalkey');
     })
 });
