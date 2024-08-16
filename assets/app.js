65e700058ea77666860c26858bbd6c33e1b6f3d6
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2024-08-16 15:58:10 -0500

    Testing a field inside an object that's persisted.

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,6 @@
 
 document.addEventListener('alpine:init', () => {
     Alpine.store('mylocalkey', {
-        txt: Alpine.$persist('Nothing here yet.').as('mylocalkey'),
+        txt: Alpine.$persist({inner: 'Blank'}).as('mylocalkey'),
     })
 });
