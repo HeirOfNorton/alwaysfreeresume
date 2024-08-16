495b241abb203d77d37a7710dcc78ff0655c5839
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2024-08-16 13:38:11 -0500

    Okay, trying it through the alpine.store mechanism.

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,8 @@
 // Site code goes here
+
+
+document.addEventListener('alpine:init', () => {
+    Alpine.store('mylocalkey', {
+        txt: Alpine.$persist('Nothing here yet.').as('mylocalkey')
+    });
+});
