505d0a8f7004b1d6d7eed54151c093728a355077
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2024-08-16 15:49:59 -0500

    Testing it as a bare object one last time.

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,5 @@
 
 
 document.addEventListener('alpine:init', () => {
-    Alpine.store('mylocalkey', {
-        txt: Alpine.$persist('Nothing here yet.').as('mylocalkey'),
-    })
+    Alpine.store('mylocalkey', Alpine.$persist('Nothing here yet.').as('mylocalkey'))
 });
