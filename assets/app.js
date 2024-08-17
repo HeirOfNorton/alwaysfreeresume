51f2c207cede28c5969df58be7a642c76fecb952
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2024-08-17 12:21:13 -0500

    Testing something, to see if I can just do object directly in $persist without using $store, when I don't need to access the silly thing more than once in a page.

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,44 @@
 // Site code goes here
 
+function createRandomString(length) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+}
+
+function blankContact() {
+    return {
+        skipped: false,
+        title: 'Contact',
+        fullname: '',
+        address: {
+            line1: '',
+            line2: '',
+            city: '',
+            state: '',
+            zip: ''
+        },
+        phone: '',
+        email: '',
+        website: ''
+    };
+}
+
+function blankSummary() {
+    return {
+        skipped: false,
+        title: 'Summary',
+        contents: ''
+    };
+}
+
+
 
 document.addEventListener('alpine:init', () => {
-    Alpine.store('mylocalkey', {
-        txt: Alpine.$persist({inner: 'Blank'}).as('mylocalkey'),
-    })
+    //Alpine.store('mylocalkey', {
+    //    txt: Alpine.$persist({inner: 'Blank'}).as('mylocalkey'),
+    //})
 });
