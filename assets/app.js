00d6918a96e169d28a143a8945bddddd67953ff0
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2024-08-18 01:27:09 -0500

    Added all the stuff for the Items form, I hope.

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,7 @@ function createRandomString(length) {
     return result;
 }
 
-function blankContact(title, preskip=false) {
+function blankContact(title, preskip) {
     return {
         skipped: preskip,
         title: title,
@@ -27,7 +27,7 @@ function blankContact(title, preskip=false) {
     };
 }
 
-function blankSummary(title, preskip=false) {
+function blankSummary(title, preskip) {
     return {
         skipped: preskip,
         title: title,
@@ -42,11 +42,11 @@ function addListItem(list, idx) {
     });
 }
 
-function delListItem(list, idx) {
+function delItem(list, idx) {
     list.items.splice(idx, 1);
 }
 
-function blankList(title, preskip=false) {
+function blankList(title, preskip) {
     var lst = {
         skipped: preskip,
         title: title,
@@ -56,18 +56,82 @@ function blankList(title, preskip=false) {
     return lst;
 }
 
-function blankItems(title, preskip=false) {
+function addFullItem(list, idx) {
+    var newitem = {
+        key: createRandomString(20)
+        title: '',
+        org: '',
+        loc: '',
+        dates: {
+            start: '',
+            end: '',
+            current: false
+        },
+        subitems: [{
+            contents: '',
+            key: createRandomString(20)
+        }]
+    };
+    list.items.splice(idx, 0, newitem);
+}
+
+function addSubItem(list, itemidx, subidx) {
+    list.items[itemidx].subitems.splice(
+        subidx, 0, {
+            contents: '',
+            key: createRandomString(20)
+        }
+    );
+}
+
+function delSubItem(list, itemidx, subidx) {
+    list.items[itemidx].subitems.splice(subidx, 1);
+}
+
+function blankItems(title, preskip, subtitle) {
+    var itemlist = {
+        skipped: preskip,
+        title: title,
+        subtitle: subtitle,
+        items: []
+    }
+    addFullItem(itemlist, 0);
+    return itemlist;
+}
+
+function blankPhoto(title, preskip) {
     return {
         skipped: preskip,
         title: title,
     };
 }
 
-function blankPhoto(title, preskip=false) {
-    return {
-        skipped: preskip,
-        title: title,
-    };
+function blankForm(title, preskip, subtitle='') {
+    switch (title) {
+        case 'Contact':
+            return blankContact(title, preskip);
+            break;
+        case 'Summary':
+            return blankSummary(title, preskip);
+            break;
+        case 'Photograph':
+            return blankPhoto(title, preskip);
+            break;
+        case 'Skills':
+        case 'Languages':
+            return blankList(title, preskip);
+            break;
+        case 'Experience':
+        case 'Education':
+        case 'Professional Skills':
+        case 'Projects':
+        case 'Activities':
+        case 'Certifications':
+        case 'Awards':
+            return blankItems(title, preskip, subtitle);
+        default:
+            alert('Something went wrong here');
+    }
 }
 
 document.addEventListener('alpine:init', () => {
